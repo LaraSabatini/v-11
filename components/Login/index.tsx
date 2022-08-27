ed28a1bddf6517e346de886dbce2d1b05713fc41
Lara <sabatinilara@gmail.com>
2022-08-27 11:58:15 -0300

    style: added styles to login page

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,15 +1,23 @@
 import React, { useState, useRef } from "react"
 import { useRouter } from "next/router"
 import validateUser from "services/ValidateUser.service"
+import theme from "theme/index"
 import TextField from "components/UI/TextField"
 import ErrorMessage from "components/UI/ErrorMessage"
 import texts from "strings/loginPage.json"
+import ShapeOne from "components/UI/Assets/images/ShapeOne"
+import ShapeSix from "components/UI/Assets/images/ShapeSix"
+import ShapeFive from "components/UI/Assets/images/ShapeFive"
+import ShapeThree from "components/UI/Assets/images/ShapeThree"
+import ShapeTwo from "components/UI/Assets/images/ShapeTwo"
+import ShapeFour from "components/UI/Assets/images/ShapeFour"
 import {
   MainContainer,
   FormContainer,
   InputContainer,
   LoginButton,
   EmptyDiv,
+  Shapes,
 } from "./styles"
 
 function Login() {
@@ -61,39 +69,70 @@ function Login() {
   }
 
   return (
-    <MainContainer>
-      <FormContainer>
-        <h2>{texts.title}</h2>
-        <InputContainer>
-          <TextField
-            label={texts.userLabel}
-            required
-            width={290}
-            type="text"
-            value={userName}
-            onChange={e => handleUserName(e)}
-            reference={userNameRef}
-          />
-          <TextField
-            label={texts.passwordLabel}
-            required
-            width={290}
-            type="password"
-            value={password}
-            onChange={e => handlePassword(e)}
-            reference={passwordRef}
-          />
-          {!success && success !== null ? (
-            <ErrorMessage message={texts.errorMessage} />
-          ) : (
-            <EmptyDiv />
-          )}
-          <LoginButton ref={submitRef} onClick={submitLogin}>
-            {texts.button}
-          </LoginButton>
-        </InputContainer>
-      </FormContainer>
-    </MainContainer>
+    <>
+      <Shapes>
+        <div className="shape-1">
+          <ShapeOne color={theme.colors.secondary} />
+        </div>
+        <div className="shape-2">
+          <ShapeTwo color={theme.colors.primary} />
+        </div>
+        <div className="shape-3">
+          <ShapeThree color={theme.colors.primary} />
+        </div>
+        <div className="shape-4">
+          <ShapeFour color={theme.colors.green} />
+        </div>
+        <div className="shape-5">
+          <ShapeFive color={theme.colors.green} />
+        </div>
+        <div className="shape-6">
+          <ShapeSix color={theme.colors.black} />
+        </div>
+        <div className="shape-7">
+          <ShapeOne color={theme.colors.secondary} />
+        </div>
+        <div className="shape-8">
+          <ShapeSix color={theme.colors.black} />
+        </div>
+        <div className="shape-9">
+          <ShapeTwo color={theme.colors.primary} />
+        </div>
+      </Shapes>
+      <MainContainer>
+        <FormContainer>
+          <h2>{texts.title}</h2>
+          <InputContainer>
+            <TextField
+              label={texts.userLabel}
+              required
+              width={290}
+              type="text"
+              value={userName}
+              onChange={e => handleUserName(e)}
+              reference={userNameRef}
+            />
+            <TextField
+              label={texts.passwordLabel}
+              required
+              width={290}
+              type="password"
+              value={password}
+              onChange={e => handlePassword(e)}
+              reference={passwordRef}
+            />
+            {!success && success !== null ? (
+              <ErrorMessage message={texts.errorMessage} />
+            ) : (
+              <EmptyDiv />
+            )}
+            <LoginButton ref={submitRef} onClick={submitLogin}>
+              {texts.button}
+            </LoginButton>
+          </InputContainer>
+        </FormContainer>
+      </MainContainer>
+    </>
   )
 }
 
--- components/Login/styles.tsx
+++ components/Login/styles.tsx
@@ -23,7 +23,7 @@ const FormContainer = styled.div`
   padding: 20px;
 
   h2 {
-    font-family: "Roboto";
+    font-family: "Josefin Sans";
     font-weight: 500;
     font-size: 20px;
     margin: 0;
@@ -42,7 +42,7 @@ const LoginButton = styled.button`
   border: none;
   outline: none;
   border-radius: 5px;
-  background-color: #0f006b;
+  background-color: ${theme.colors.primary};
   color: ${theme.colors.white};
   font-family: ${theme.fonts.primary};
   font-weight: ${theme.fontWeights.semiBold};
@@ -53,4 +53,65 @@ const EmptyDiv = styled.div`
   height: 11px;
 `
 
-export { FormContainer, MainContainer, InputContainer, LoginButton, EmptyDiv }
+const Shapes = styled.div`
+  position: absolute;
+  div {
+    position: absolute;
+  }
+
+  .shape-1 {
+    top: 100px;
+    left: 50px;
+  }
+
+  .shape-2 {
+    left: 500px;
+    top: 20px;
+  }
+
+  .shape-3 {
+    left: 1000px;
+    top: 500px;
+  }
+
+  .shape-4 {
+    left: 100px;
+    top: 600px;
+  }
+
+  .shape-5 {
+    left: 1100px;
+    top: 150px;
+  }
+
+  .shape-6 {
+    left: 250px;
+    top: 300px;
+  }
+
+  .shape-7 {
+    left: 1150px;
+    top: 700px;
+  }
+
+  .shape-8 {
+    left: 800px;
+    top: 50px;
+    transform: rotate(180deg);
+  }
+
+  .shape-9 {
+    left: 650px;
+    top: 700px;
+    transform: rotate(90deg);
+  }
+`
+
+export {
+  FormContainer,
+  MainContainer,
+  InputContainer,
+  LoginButton,
+  EmptyDiv,
+  Shapes,
+}
