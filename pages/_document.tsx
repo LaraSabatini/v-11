a37422ddb07de7a1b423743801aa35f16f1b0d17
Lara <sabatinilara@gmail.com>
2022-08-28 13:37:15 -0300

    added overlay to document file-

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id="overlay" />
         </body>
       </Html>
     )
