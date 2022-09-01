a7ad32f40fd11f0ccd24fa81587b25ae840e8762
Lara <sabatinilara@gmail.com>
2022-09-01 17:00:32 -0300

    testing;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import type { AppProps } from "next/app"
+// import type { AppProps } from "next/app"
 import { useEffect } from "react"
 import { useRouter } from "next/router"
 import useStorage from "../hooks/useStorage"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   const { getItem } = useStorage()
   const logged = getItem("isLoggedIn")
   const router = useRouter()
