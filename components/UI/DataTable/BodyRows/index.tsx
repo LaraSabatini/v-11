efa80df92f91c6b53dcff2b500e5fafb23b44e9d
Lara <sabatinilara@gmail.com>
2022-09-06 16:54:36 -0300

    feat: created dataTable component

---
+++ components/UI/DataTable/BodyRows/index.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/no-array-index-key */
+import React from "react"
+import { TableRow, RowCell, Cell } from "./styles"
+import StatusIcon from "../../StatusIcon"
+
+export interface RowInterface {
+  columns: {
+    id: number
+    field: string
+    header_name: string
+    width: "s" | "m" | "l" | "xl"
+  }[]
+  item: any
+  onRowClick?: (arg?: any) => void
+}
+
+const BodyRows: React.FC<RowInterface> = ({ item, columns, onRowClick }) => {
+  if (item.status && item.status.status && item.status.status === "REVIEWING") {
+    return (
+      <TableRow
+        status={item.status.status}
+        onClick={onRowClick}
+        data-id={item.id}
+      >
+        {columns.map((columnItem, index) => {
+          return (
+            <RowCell key={index} width={columnItem.width}>
+              <Cell className="cell">
+                {columnItem.field === "status" ? (
+                  <StatusIcon
+                    status={item[`${columnItem.field}`].status}
+                    hasTooltip
+                    tooltipText={item[`${columnItem.field}`].tooltip_text}
+                  />
+                ) : (
+                  <p className="cell-content">{item[`${columnItem.field}`]}</p>
+                )}
+              </Cell>
+              <div className="cell-divider" />
+            </RowCell>
+          )
+        })}
+      </TableRow>
+    )
+  }
+
+  return (
+    <TableRow
+      onClick={onRowClick}
+      status={item.status && item.status.status}
+      data-id={item.id}
+    >
+      {columns.map((columnItem, index) => {
+        return (
+          <RowCell key={index} width={columnItem.width}>
+            <Cell className="cell">
+              {columnItem.field === "status" ? (
+                <StatusIcon
+                  status={item[`${columnItem.field}`].status}
+                  hasTooltip
+                  tooltipText={item[`${columnItem.field}`].tooltip_text}
+                />
+              ) : (
+                <p className="cell-content">{item[`${columnItem.field}`]}</p>
+              )}
+            </Cell>
+            <div className="cell-divider" />
+          </RowCell>
+        )
+      })}
+    </TableRow>
+  )
+}
+
+export default BodyRows
---
+++ components/UI/DataTable/index.tsx
@@ -0,0 +1,134 @@
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from "react"
+import Pagination from "components/UI/Pagination"
+import ScrollView from "components/UI/ScrollView"
+import Icon from "components/UI/Assets/Icon"
+import {
+  TableWrapper,
+  TableContainer,
+  TableHeader,
+  HeaderCell,
+  TableBody,
+  TableDivider,
+  PaginatorContainer,
+  NotFoundContainer,
+} from "./styles"
+import BodyRows from "./BodyRows"
+
+export interface TableInterface {
+  columns: {
+    id: number
+    field: string
+    header_name: string
+    width: "s" | "m" | "l" | "xl"
+  }[]
+  rows: {
+    success: boolean
+    message?: {
+      icon: string
+      content: string
+    }
+    rows: any[]
+  }
+  height: number
+  onRowClick?: (arg?: any) => void
+  onClickNext?: (arg?: any) => void
+  onClickBack?: (arg?: any) => void
+  onClickLast?: (arg?: any) => void
+  setPage?: number
+  reference?: any
+  totalPages?: number
+  noPaginator?: boolean
+}
+
+const DataTableComponent: React.FC<TableInterface> = ({
+  columns,
+  rows,
+  height,
+  onRowClick,
+  onClickNext,
+  onClickBack,
+  onClickLast,
+  totalPages,
+  setPage,
+  reference,
+  noPaginator,
+}) => {
+  const [selectedRow, setSelectedRow] = useState<number>(null)
+  const handleRowClick = (item: any, e: any) => {
+    if (onRowClick !== undefined) {
+      setSelectedRow(item.id)
+      setTimeout(() => {
+        onRowClick({ e, item })
+      }, 10)
+    }
+  }
+
+  const calculateMinWidth = (): number => {
+    let count = 0
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < columns?.length; i++) {
+      if (columns[i].width === "s") {
+        count += 62
+      } else {
+        count += 160
+      }
+    }
+    return count
+  }
+
+  const minWidth = calculateMinWidth()
+
+  return (
+    <TableWrapper>
+      <ScrollView height={height} horizontal>
+        <TableContainer minWidth={minWidth}>
+          <TableHeader>
+            {columns?.map((column, index) => (
+              <HeaderCell key={index} width={column.width}>
+                <p>{column.header_name}</p>
+              </HeaderCell>
+            ))}
+          </TableHeader>
+          <TableBody ref={reference} data-id={selectedRow}>
+            {rows?.success && rows.rows?.length ? (
+              rows.rows.map((item, index) => (
+                <div className="row-wrapper" key={index}>
+                  <BodyRows
+                    onRowClick={e => handleRowClick(item, e)}
+                    item={item}
+                    columns={columns}
+                  />
+                  <hr className="row-divider" />
+                </div>
+              ))
+            ) : (
+              <NotFoundContainer>
+                <Icon icon={rows?.message?.icon} />
+                <h3 className="content">{rows?.message?.content}</h3>
+              </NotFoundContainer>
+            )}
+          </TableBody>
+        </TableContainer>
+      </ScrollView>
+      {!noPaginator || noPaginator === undefined ? (
+        <>
+          <TableDivider />
+          <PaginatorContainer>
+            <Pagination
+              setPage={setPage}
+              totalPages={totalPages}
+              onClickNext={onClickNext}
+              onClickBack={onClickBack}
+              onClickLastPage={onClickLast}
+            />
+          </PaginatorContainer>
+        </>
+      ) : (
+        <></>
+      )}
+    </TableWrapper>
+  )
+}
+
+export default DataTableComponent
