422851734f06aa9022971d94a1ccfe4288e7efa1
Lara <sabatinilara@gmail.com>
2022-09-20 11:23:58 -0300

    fixed bug for payments

---
+++ interfaces/finances/DigitalPayments.ts
@@ -0,0 +1,12 @@
+export default interface DigitalPaymentsInterface {
+  id: number
+  user_id: number
+  user_name: string
+  product_id: number
+  product_name: string
+  date: string
+  month: string
+  month_id: number
+  amount_of_products: number
+  total_profit: number
+}
