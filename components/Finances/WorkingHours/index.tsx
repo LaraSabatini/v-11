21335c736bbf14aff785fd0234d9a1ba0699718d
Lara <sabatinilara@gmail.com>
2022-10-18 19:55:41 -0300

    commented unused code

--- components/Finances/WorkingHours/index.tsx
+++ components/Finances/WorkingHours/index.tsx
@@ -1,25 +1,25 @@
-import React, { useContext, useEffect } from "react"
+// import React, { useContext, useEffect } from "react"
 // DATA STORAGE & TYPES
-import { WorkingHoursContext } from "contexts/WorkingHours"
+// import { WorkingHoursContext } from "contexts/WorkingHours"
 
 const WorkingHours = () => {
-  const {
-    // actualWeek, setActualWeek
-    weekSelected,
-  } = useContext(WorkingHoursContext)
+  // const {
+  //   // actualWeek, setActualWeek
+  //   weekSelected,
+  // } = useContext(WorkingHoursContext)
 
-  const calculateActualWeek = () => {
-    // const currentDate = new Date()
-    // const startDate = new Date(currentDate.getFullYear(), 0, 1)
-    // const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000))
-    // const weekNumber = Math.ceil(days / 7)
-    // setActualWeek(weekNumber)
-  }
+  // const calculateActualWeek = () => {
+  // const currentDate = new Date()
+  // const startDate = new Date(currentDate.getFullYear(), 0, 1)
+  // const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000))
+  // const weekNumber = Math.ceil(days / 7)
+  // setActualWeek(weekNumber)
+  // }
 
-  useEffect(() => {
-    calculateActualWeek()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [weekSelected])
+  // useEffect(() => {
+  //   calculateActualWeek()
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [weekSelected])
 
   /*
     DIVIDIR POR SEMANA => func para obtener el numero de semana que es
