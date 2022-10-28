b3b0c79c6fe2ec74e46993c23062990b5184abcf
Lara <sabatinilara@gmail.com>
2022-10-27 21:33:18 -0300

    trial of simplifing complexity

--- components/Trainers/Forms/CreatePurchase/index.tsx
+++ components/Trainers/Forms/CreatePurchase/index.tsx
@@ -3,8 +3,6 @@
 import React, { useState, useContext, useEffect } from "react"
 // SERVICES
 import { searchPartner } from "services/Partners/Partner.service"
-import { getPartnerPaymentsById } from "services/Partners/PartnerPayments.service"
-import { getLessonsByDateAndShift } from "services/Trainers/LessonsPurchased.service"
 // DATA STORAGE & TYPES
 import { paymentMethods, paymentUsers } from "const/finances"
 import yesOrNoArr from "const/fixedVariables"
@@ -22,6 +20,10 @@ import InputCalendar from "components/UI/InputCalendar"
 import Icon from "components/UI/Assets/Icon"
 import PopOver from "components/UI/PopOver"
 import ScrollView from "components/UI/ScrollView"
+import deleteLessonFromList from "./utlis/deleteLessonFromList"
+import checkIfDateHasSpace from "./utlis/checkIsDateHasSpace"
+import calculatePriceWithoutDiscount from "./utlis/calculatePriceWithoutDiscount"
+import checkDiscount from "./utlis/checkDiscount"
 import {
   FormContainer,
   HorizontalGroup,
@@ -111,112 +113,41 @@ function CreatePurchaseModal({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchValue])
 
-  const deleteLessonFromList = (lesson: {
-    id: number
-    date: string
-    shift: string
-  }) => {
-    const newArrayOfDates = datesSelected.filter(
-      (lessonDate: { id: number; date: string; shift: "AM" | "PM" }) =>
-        lessonDate !== lesson,
+  const setFinalPriceFunction = () => {
+    const calcFinalPrice = calculatePriceWithoutDiscount(
+      amountOfLessons,
+      paymentMethodSelected.id,
+      prices,
     )
-    setDatesSelected(newArrayOfDates)
+    setFinalPrice(calcFinalPrice)
   }
 
-  const checkIfDateHasSpace = async () => {
-    const dateCleaned = `${provisionalSelection.date.slice(
-      0,
-      2,
-    )}-${provisionalSelection.date.slice(
-      3,
-      5,
-    )}-${provisionalSelection.date.slice(6, 10)}`
-
-    const checkAvailability = await getLessonsByDateAndShift(
-      dateCleaned,
-      provisionalSelection.shift,
-    )
-
-    if (checkAvailability.data.length >= 10) {
-      setCannotAddDate(true)
-    } else {
-      setCannotAddDate(false)
-      setDatesSelected([
-        ...datesSelected,
-        {
-          id: datesSelected.length + 1,
-          date: provisionalSelection.date,
-          shift: provisionalSelection.shift,
-        },
-      ])
-
-      setProvisionalSelection({
-        date: "",
-        shift: "",
-      })
-    }
-  }
-
-  const calculatePriceWithoutDiscount = () => {
-    let price: number = 0
-    if (amountOfLessons === 4) {
-      price =
-        paymentMethodSelected.id === 1
-          ? prices[4].price_cash
-          : prices[4].price_mp
-    } else if (amountOfLessons === 8) {
-      price =
-        paymentMethodSelected.id === 1
-          ? prices[5].price_cash
-          : prices[5].price_mp
-    } else {
-      price =
-        paymentMethodSelected.id === 1
-          ? prices[3].price_cash * amountOfLessons
-          : prices[3].price_mp * amountOfLessons
-    }
-    setFinalPrice(price)
-  }
+  const lessonPriceForFreePass: {
+    amount_of_lessons: number
+    cash: number
+    mp: number
+  }[] = [
+    {
+      amount_of_lessons: 1,
+      cash: prices[3].price_cash - prices[0].price_cash,
+      mp: prices[3].price_mp - prices[0].price_mp,
+    },
+    {
+      amount_of_lessons: 4,
+      cash: prices[4].price_cash - prices[0].price_cash * 4,
+      mp: prices[4].price_mp - prices[0].price_mp * 4,
+    },
+    {
+      amount_of_lessons: 8,
+      cash: prices[5].price_cash - prices[0].price_cash * 8,
+      mp: prices[5].price_mp - prices[0].price_mp * 8,
+    },
+  ]
 
   const calculatePrice = async () => {
-    const lessonPriceForFreePass: {
-      amount_of_lessons: number
-      cash: number
-      mp: number
-    }[] = [
-      {
-        amount_of_lessons: 1,
-        cash: prices[3].price_cash - prices[0].price_cash,
-        mp: prices[3].price_mp - prices[0].price_mp,
-      },
-      {
-        amount_of_lessons: 4,
-        cash: prices[4].price_cash - prices[0].price_cash * 4,
-        mp: prices[4].price_mp - prices[0].price_mp * 4,
-      },
-      {
-        amount_of_lessons: 8,
-        cash: prices[5].price_cash - prices[0].price_cash * 8,
-        mp: prices[5].price_mp - prices[0].price_mp * 8,
-      },
-    ]
-
     if (clientSelected !== null && clientSelected.free_pass === 1) {
-      const checkPayment = await getPartnerPaymentsById(clientSelected.id)
+      const hasDiscount = await checkDiscount(clientSelected.id)
 
-      const expirationDate =
-        checkPayment.data[checkPayment.data.length - 1].payment_expire_date
-
-      const expirationDay = expirationDate.slice(0, 2)
-      const expirationMonth = expirationDate.slice(3, 5)
-      const expirationYear = expirationDate.slice(6, 10)
-
-      const expirationDateCleaned = new Date(
-        `${expirationYear}-${expirationMonth}-${expirationDay}`,
-      )
-      const todayDate = new Date(`${year}-${month}-${day}`)
-
-      const hasDiscount: boolean = expirationDateCleaned > todayDate
       let price: number = 0
 
       if (hasDiscount) {
@@ -236,12 +167,12 @@ function CreatePurchaseModal({
               ? lessonPriceForFreePass[0].cash * amountOfLessons
               : lessonPriceForFreePass[0].mp * amountOfLessons
         }
+        setFinalPrice(price)
       } else {
-        calculatePriceWithoutDiscount()
+        setFinalPriceFunction()
       }
-      setFinalPrice(price)
     } else {
-      calculatePriceWithoutDiscount()
+      setFinalPriceFunction()
     }
   }
 
@@ -307,7 +238,6 @@ function CreatePurchaseModal({
       }
     >
       <FormContainer>
-        {/* START - CLIENT IS REGISTERED VALIDATION */}
         <HorizontalGroup>
           <Autocomplete
             label={trainerTexts.createPurchase.registeredLabel}
@@ -381,9 +311,7 @@ function CreatePurchaseModal({
             </>
           )}
         </Results>
-        {/* END - CLIENT IS REGISTERED VALIDATION */}
 
-        {/* START - REGISTER CLIENT */}
         {!clientIsRegistered && clientIsRegistered !== null && (
           <RegisterClientContainer>
             <HorizontalGroup>
@@ -461,7 +389,6 @@ function CreatePurchaseModal({
           </RegisterClientContainer>
         )}
 
-        {/* START - CLIENT IS REGISTERED VALIDATION */}
         {(clientSelected !== null || clientIsRegistered === false) && (
           <div className="subdiv">
             <LessonsContainer>
@@ -527,12 +454,26 @@ function CreatePurchaseModal({
                     provisionalSelection.date === "" ||
                     provisionalSelection.shift === ""
                   }
-                  onClick={() => {
+                  onClick={async () => {
                     if (
                       provisionalSelection.date !== "" &&
                       provisionalSelection.shift !== ""
                     ) {
-                      checkIfDateHasSpace()
+                      const checkDate = await checkIfDateHasSpace(
+                        provisionalSelection,
+                        datesSelected,
+                      )
+
+                      if (checkDate.can) {
+                        setDatesSelected(checkDate.newDates)
+                        setProvisionalSelection({
+                          date: "",
+                          shift: "",
+                        })
+                        setCannotAddDate(false)
+                      } else {
+                        setCannotAddDate(true)
+                      }
                     }
                   }}
                 >
@@ -552,7 +493,13 @@ function CreatePurchaseModal({
                         <span>• {date.date}</span> <b>{date.shift}</b>
                         <button
                           type="button"
-                          onClick={() => deleteLessonFromList(date)}
+                          onClick={() => {
+                            const newArrayOfDates = deleteLessonFromList(
+                              date,
+                              datesSelected,
+                            )
+                            setDatesSelected(newArrayOfDates)
+                          }}
                         >
                           <Icon icon="IconMenuOff" />
                         </button>
---
+++ components/Trainers/Forms/CreatePurchase/utlis/checkDiscount.ts
@@ -0,0 +1,22 @@
+import { getPartnerPaymentsById } from "services/Partners/PartnerPayments.service"
+import { day, month, year } from "const/time"
+
+const checkDiscount = async (clientId: number) => {
+  const checkPayment = await getPartnerPaymentsById(clientId) // clientSelected.id
+
+  const expirationDate =
+    checkPayment.data[checkPayment.data.length - 1].payment_expire_date
+
+  const expirationDay = expirationDate.slice(0, 2)
+  const expirationMonth = expirationDate.slice(3, 5)
+  const expirationYear = expirationDate.slice(6, 10)
+
+  const expirationDateCleaned = new Date(
+    `${expirationYear}-${expirationMonth}-${expirationDay}`,
+  )
+  const todayDate = new Date(`${year}-${month}-${day}`)
+
+  return expirationDateCleaned > todayDate
+}
+
+export default checkDiscount
