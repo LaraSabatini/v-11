e564bb35cbd5ff31457f97bd54eb8d9f9acce147
Lara <sabatinilara@gmail.com>
2022-11-10 15:26:44 -0300

    added input in edit note

--- components/Annotations/EditAnnotation/index.tsx
+++ components/Annotations/EditAnnotation/index.tsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect } from "react"
 import ModalForm from "components/UI/ModalForm"
 import { AnnotationsContext } from "contexts/Annotations"
 import TextField from "components/UI/TextField"
+import Autocomplete from "components/UI/Autocomplete"
+import { annotationTypes } from "const/annotations"
+import HorizontalContainer from "../CreateAnnotation/styles"
 
 interface EditAnnotationInterface {
   submitAction: () => void
@@ -12,9 +15,14 @@ function EditAnnotation({
   submitAction,
   cancelAction,
 }: EditAnnotationInterface) {
-  const { annotationSelected, setNewAnnotation, newAnnotation } = useContext(
-    AnnotationsContext,
-  )
+  const {
+    annotationSelected,
+    setNewAnnotation,
+    newAnnotation,
+    typeRef,
+    titleRef,
+    descriptionRef,
+  } = useContext(AnnotationsContext)
 
   useEffect(() => {
     setNewAnnotation(annotationSelected)
@@ -32,22 +40,36 @@ function EditAnnotation({
       cancelFunction={cancelAction}
     >
       <>
-        <TextField
-          label="Titulo"
-          required
-          type="text"
-          width={230}
-          value={newAnnotation.title}
-          onChange={e =>
-            setNewAnnotation({ ...newAnnotation, title: e.target.value })
-          }
-        />
+        <HorizontalContainer>
+          <TextField
+            label="Titulo"
+            required
+            type="text"
+            width={230}
+            reference={titleRef}
+            value={newAnnotation.title}
+            onChange={e =>
+              setNewAnnotation({ ...newAnnotation, title: e.target.value })
+            }
+          />
+          <Autocomplete
+            label="Tipo"
+            required
+            ref={typeRef}
+            options={annotationTypes}
+            width={140}
+            onChangeProps={e =>
+              setNewAnnotation({ ...newAnnotation, type: e.display_name })
+            }
+          />
+        </HorizontalContainer>
         <TextField
           label="Descripcion"
           max={200}
           required
           type="textarea"
           width={380}
+          reference={descriptionRef}
           value={newAnnotation.description}
           onChange={e =>
             setNewAnnotation({ ...newAnnotation, description: e.target.value })
