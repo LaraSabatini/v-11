844a67a0378af12f4c2ac0a8f4a2f21376dedd73
Lara <sabatinilara@gmail.com>
2022-11-26 11:13:42 -0300

    changed logic of finances

--- components/Finances/Till/BoulderView/index.tsx
+++ components/Finances/Till/BoulderView/index.tsx
@@ -1,12 +1,7 @@
-import React, { useContext, useState, useEffect } from "react"
+import React from "react"
 // DATA STORAGE & TYPES
-import { Finances } from "contexts/Finances"
 import financesTexts from "strings/finances.json"
 import generalTexts from "strings/general.json"
-import reestructureDataForDays from "./helpers/reestructureDataForDays"
-import setLessonsData from "./helpers/setLessonsData"
-import fillDataForOtherThanDays from "./helpers/fillDataForOtherThanDays"
-import fillDataForRentedShoes from "./helpers/fillDataForRentedShoes"
 // COMPONENTS & STYLING
 import FreePassCard from "./FreePassCard"
 import LessonsCard from "./LessonsCard"
@@ -19,110 +14,54 @@ import {
   Earnings,
 } from "./styles"
 
-function BoulderView() {
-  const {
-    boulderProductsPurchasedByDate,
-    partnerPaymentsByDate,
-    boulderPurchasesViewData,
-    setBoulderPurchasesViewData,
-    finalEargninsBoulder,
-    setFinalEargninsBoulder,
-  } = useContext(Finances)
+interface BoulderViewInterface {
+  data: any
+}
 
-  const [daysPurchased, setDaysPurchased] = useState({
-    store: 0,
-    partner: 0,
-  })
-
-  const [dayPacks, setDayPacks] = useState<number[]>([])
-  const [lessonPacks, setLessonPacks] = useState<number[]>([])
-
-  const setDataForDays = () => {
-    const data = reestructureDataForDays(
-      partnerPaymentsByDate,
-      boulderProductsPurchasedByDate,
-      boulderPurchasesViewData,
-    )
-    setDayPacks(data.dayPacks)
-    setDaysPurchased(data.daysPurchased)
-  }
-
-  const setFinalData = () => {
-    const finalData = fillDataForRentedShoes(
-      boulderProductsPurchasedByDate,
-      boulderPurchasesViewData,
-    )
-
-    setFinalEargninsBoulder(finalData.finalEarningsBoulder)
-    setBoulderPurchasesViewData(finalData.boulderPurchasesViewData)
-  }
-
-  useEffect(() => {
-    setBoulderPurchasesViewData(
-      fillDataForOtherThanDays(partnerPaymentsByDate, boulderPurchasesViewData),
-    )
-    setFinalData()
-    setDataForDays()
-    setLessonPacks(setLessonsData(partnerPaymentsByDate))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [boulderProductsPurchasedByDate, partnerPaymentsByDate])
-
-  useEffect(() => {
-    setBoulderPurchasesViewData(
-      fillDataForOtherThanDays(partnerPaymentsByDate, boulderPurchasesViewData),
-    )
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [partnerPaymentsByDate, boulderPurchasesViewData])
-
+function BoulderView({ data }: BoulderViewInterface) {
   return (
     <Container>
       <FinalProfit>
         <p>
-          <span>{generalTexts.payments.cash}:</span>{" "}
-          <b>$ {finalEargninsBoulder.cash}</b>
+          <span>{generalTexts.payments.cash}:</span> <b>$ {data.cash}</b>
         </p>
         <p>
-          <span>{generalTexts.payments.digital}:</span>{" "}
-          <b>$ {finalEargninsBoulder.mp}</b>
+          <span>{generalTexts.payments.digital}:</span> <b>$ {data.mp}</b>
         </p>
       </FinalProfit>
       <CardsContainer>
         <FreePassCard
-          earningsCash={boulderPurchasesViewData[0].earnings_cash}
-          earningsDigital={boulderPurchasesViewData[0].earnings_mp}
-          amountOfSells={boulderPurchasesViewData[0].amount_of_days_sold}
-          byStore={daysPurchased.store}
-          byClient={daysPurchased.partner}
-          packs={dayPacks}
-          people={partnerPaymentsByDate.length + daysPurchased.store}
+          earningsCash={data.freePass.earnings.cash}
+          earningsDigital={data.freePass.earnings.cash}
+          amountOfSells={data.freePass.total}
+          byStore={data.freePass.individual}
+          byClient={data.freePass.packFour * 4 + data.freePass.packEight * 8}
+          packFour={data.freePass.packFour}
+          packEight={data.freePass.packEight}
+          people={data.freePass.amountOfPeople}
         />
         <LessonsCard
-          earningsCash={boulderPurchasesViewData[3].earnings_cash}
-          earningsDigital={boulderPurchasesViewData[3].earnings_mp}
-          amountOfSells={boulderPurchasesViewData[3].amount_of_lessons_sold}
-          packs={lessonPacks}
-          nonPacks={
-            boulderPurchasesViewData[3].amount_of_lessons_sold -
-            (lessonPacks[0] * 4 + lessonPacks[1] * 8)
-          }
+          earningsCash={data.lessons.earnings.cash}
+          earningsDigital={data.lessons.earnings.mp}
+          amountOfSells={data.lessons.total}
+          packFour={data.lessons.packFour}
+          packEight={data.lessons.packEight}
+          nonPacks={data.lessons.individual}
         />
 
         <Card>
           <Title className="month">{financesTexts.month.toUpperCase()}</Title>
           <Earnings>
             <p>
-              {generalTexts.payments.cash}:{" "}
-              <b>$ {boulderPurchasesViewData[1].earnings_cash}</b>
+              {generalTexts.payments.cash}: <b>$ {data.month.earnings.cash}</b>
             </p>
             <p>
-              {generalTexts.payments.digital}:{" "}
-              <b>$ {boulderPurchasesViewData[1].earnings_mp}</b>
+              {generalTexts.payments.digital}: <b>$ {data.month.earnings.mp}</b>
             </p>
           </Earnings>
           <Earnings>
             <p>
-              {financesTexts.sells}:{" "}
-              <b>{boulderPurchasesViewData[1].amount_of_months_sold}</b>
+              {financesTexts.sells}: <b>{data.month.total}</b>
             </p>
           </Earnings>
         </Card>
@@ -130,18 +69,15 @@ function BoulderView() {
           <Title className="combo">{financesTexts.combo.toUpperCase()}</Title>
           <Earnings>
             <p>
-              {generalTexts.payments.cash}:{" "}
-              <b>$ {boulderPurchasesViewData[2].earnings_cash}</b>
+              {generalTexts.payments.cash}: <b>$ {data.combo.earnings.cash}</b>
             </p>
             <p>
-              {generalTexts.payments.digital}:{" "}
-              <b>$ {boulderPurchasesViewData[2].earnings_mp}</b>
+              {generalTexts.payments.digital}: <b>$ {data.combo.earnings.mp}</b>
             </p>
           </Earnings>
           <Earnings>
             <p>
-              {financesTexts.sells}:{" "}
-              <b>{boulderPurchasesViewData[2].amount_of_combos_sold}</b>
+              {financesTexts.sells}: <b>{data.combo.total}</b>
             </p>
           </Earnings>
         </Card>
@@ -149,18 +85,15 @@ function BoulderView() {
           <Title className="shoes">{financesTexts.shoes.toUpperCase()}</Title>
           <Earnings>
             <p>
-              {generalTexts.payments.cash}:{" "}
-              <b>$ {boulderPurchasesViewData[4].earnings_cash}</b>
+              {generalTexts.payments.cash}: <b>$ {data.shoes.earnings.cash}</b>
             </p>
             <p>
-              {generalTexts.payments.digital}:{" "}
-              <b>$ {boulderPurchasesViewData[4].earnings_mp}</b>
+              {generalTexts.payments.digital}: <b>$ {data.shoes.earnings.mp}</b>
             </p>
           </Earnings>
           <Earnings>
             <p className="asistencies">
-              {financesTexts.units_lend}:{" "}
-              <b>{boulderPurchasesViewData[4].amount_of_shoes_rented}</b>
+              {financesTexts.units_lend}: <b>{data.shoes.total}</b>
             </p>
           </Earnings>
         </Card>
