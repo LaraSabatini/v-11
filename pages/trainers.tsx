6dd823dda54637e7c1482f1db016efe787a57456
Lara <sabatinilara@gmail.com>
2022-11-30 15:25:22 -0300

    removed old folder

--- pages/trainers.tsx
+++ pages/trainers.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from "react"
-// import TrainersView from "components/Trainers"
-import TrainersView from "components/TrainersView"
+import TrainersView from "components/Trainers"
 import LessonsProvider from "@contexts/Lessons"
 import { GeneralContext } from "contexts/GeneralContext"
 import { getPricesAction } from "helpers/partners"
