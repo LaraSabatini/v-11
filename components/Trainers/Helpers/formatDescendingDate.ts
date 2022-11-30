987f467fbfcc03e19da81a581e3acaaec0886a42
Lara <sabatinilara@gmail.com>
2022-11-30 15:33:31 -0300

    renamed function implementation

--- components/Trainers/Helpers/formatDescendingDate.ts
+++ components/Trainers/Helpers/formatDescendingDate.ts
@@ -1,7 +1,8 @@
 const formatDescendingDate = (date: string) => {
-  return `${date.slice(6, 10)}-${date.slice(3, 5)}-${
-    parseInt(date.slice(0, 2), 10) + 1
-  }`
+  return `${date.slice(3, 5)}-${parseInt(date.slice(0, 2), 10)}-${date.slice(
+    6,
+    10,
+  )}`
 }
 
 export default formatDescendingDate
