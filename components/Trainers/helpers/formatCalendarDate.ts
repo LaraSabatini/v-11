24db50dffe90c9b8ae67c722574706685091ac92
Lara <sabatinilara@gmail.com>
2022-11-30 15:24:26 -0300

    removed old folder

--- components/Trainers/helpers/formatCalendarDate.ts
+++
@@ -1,5 +0,0 @@
-const formatCalendarDate = (date: Date) => {
-  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-}
-
-export default formatCalendarDate
--- components/Trainers/helpers/formatDescDate.ts
+++
@@ -1,7 +0,0 @@
-const formatDescDate = (date: string) => {
-  return `${date.slice(6, 10)}-${date.slice(3, 5)}-${
-    parseInt(date.slice(0, 2), 10) + 1
-  }`
-}
-
-export default formatDescDate
