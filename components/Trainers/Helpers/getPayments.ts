79db80426043ffffb8f500e442a92f0786c99097
Lara <sabatinilara@gmail.com>
2022-12-01 12:08:51 -0300

    cleaned function

--- components/Trainers/Helpers/getPayments.ts
+++ components/Trainers/Helpers/getPayments.ts
@@ -1,4 +1,5 @@
 import { getLessonsByPartnerAndPaidAction } from "helpers/lessons"
+import ClasesPurchasedInterface from "interfaces/trainers/ClasesPurchasedInterface"
 
 const getPayments = async (studentSelected: number) => {
   const lessonsPaid = await getLessonsByPartnerAndPaidAction(
@@ -10,21 +11,24 @@ const getPayments = async (studentSelected: number) => {
     "NO",
   )
 
-  const finalArr = lessonsPaid.concat(lessonsNotPaid).sort((a, b) => {
+  const arrayOfLessons = lessonsPaid.concat(lessonsNotPaid).sort((a, b) => {
     return b.id - a.id
   })
 
   const paidDays = []
-  finalArr.map(purchase => paidDays.push(purchase.paid_day))
+  arrayOfLessons.map((purchase: ClasesPurchasedInterface) =>
+    paidDays.push(purchase.paid_day),
+  )
 
-  const uniqueArray = paidDays.filter((item, pos) => {
+  const cleanedArray = paidDays.filter((item, pos) => {
     return paidDays.indexOf(item) === pos
   })
 
-  const finalArrayOfDates = uniqueArray.map(paidDate => {
+  const finalArrayOfDates = cleanedArray.map(paidDate => {
     const arrayOfDates = []
-    finalArr.filter(
-      lesson => lesson.paid_day === paidDate && arrayOfDates.push(lesson),
+    arrayOfLessons.filter(
+      (lesson: ClasesPurchasedInterface) =>
+        lesson.paid_day === paidDate && arrayOfDates.push(lesson),
     )
     return arrayOfDates
   })
